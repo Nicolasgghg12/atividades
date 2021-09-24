07e4191525b662afa4dde202376836a4c6a18ba4
Nicolasgghg12 <89709868+Nicolasgghg12@users.noreply.github.com>
2021-09-23 21:26:20 -0300

    Add files via upload

---
+++ Classificação.js
@@ -0,0 +1,12 @@
+var
+Qtal = 20;
+
+for(a=0;a<Qtal;a++){
+    if(a==0){
+        console.log("Número" + a + " é zero");
+    }else if((a % 2) == 0){
+        console.log("Número " + a + " é par");
+    }else{
+        console.log("Número " + a + " é impar");
+    }
+}
